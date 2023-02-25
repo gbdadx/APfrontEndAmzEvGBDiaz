94ac625fb6cba52d282184f428b0d68f0a9abf34
gabriela diaz <gbdadx@gmail.com>
2023-02-25 15:00:31 -0300

    changed some  finished events'style

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const todos = [];
 function probando(arreglo) {
     for (let i of e) {
         if (i.date < fechaBase) {
-            i.description = `<p style="background-color:Tomato; padding:10px; color:white">Finished </p> `;
+            i.description = `<p style="background-color:MediumSeaGreen; text-align:center;padding:10px; color:white">Finished </p> `;
         }
         arreglo.push(i);
     }
--- assets/js/past.js
+++ assets/js/past.js
@@ -5,7 +5,7 @@ const pasados = [];
 function passed(arreglo) {
     for (let i of eventos.events) {
         if (i.date < fechaBase) {
-            i.description = `<p style="background-color:Tomato; padding:10px; color:white">Finished </p> `;
+            i.description = `<p style="background-color:MediumSeaGreen; text-align:center;padding:10px; color:white">Finished </p> `;
             arreglo.push(i);
         }
     }
