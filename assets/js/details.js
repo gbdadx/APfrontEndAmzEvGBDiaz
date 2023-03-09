c3f58626fc72edac2d01488544d27cf100e370d6
gabriela diaz <gbdadx@gmail.com>
2023-03-08 23:21:46 -0300

    some interactivity to DETAILS, with js and css, display, tooltips, click, double click and refresh

--- assets/js/details.js
+++ assets/js/details.js
@@ -16,7 +16,7 @@ document.getElementById('price').innerHTML = `Price: $ ${evento.price} `;
 document.getElementById('place').innerHTML = `Place: ${evento.place}`;
 
 document.getElementById('capacity').innerHTML = `Capacity: ${evento.capacity}`;
-if (evento.assistance !==undefined) {
+if (evento.assistance !== undefined) {
     document.getElementById('assistance').innerHTML = `Assitance: ${evento.assistance}`;
 }
 
@@ -37,4 +37,32 @@ if (contElem < evento.currentDate) {
 
 
 
+/* desaparecer el detalle al hacer click en la tarjeta */
 
+
+
+function hideText() {
+    const cont2 = document.getElementById("cont2");
+    const cont1 = document.getElementById("cont1");
+    const foto = document.getElementById("foto");
+
+    cont2.style.display = "none";
+    cont1.style.width = "120%";
+    cont1.style.height = "120vh";
+    foto.style.width = "100%";
+    foto.style.height = "110vh";
+    foto.style.backgroundImage = `url(${evento.image})`;
+
+}
+function showText() {
+    const cont2 = document.getElementById("cont2");
+    cont2.style.display = "block";
+    cont2.style.position = "fixed";
+    cont2.style.zIndex=4;
+    foto.style.zIndex=-1;
+    cont2.style.backgroundColor=" rgba(255,255, 255, 0.9)";
+
+
+}
+
+/* reaparecer el detalle al hacer cllick en la imagen*/
